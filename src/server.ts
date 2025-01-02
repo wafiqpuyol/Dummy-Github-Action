9afd6db34c857555a8d53fda067211cfddd5b926
wafiq <wafiqgithub775@gmail.com>
2025-01-02 17:21:29 +0600

    add workflow endpoint

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ const app = express()
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
+app.get("/workflow", (req, res) => {
+    res.send("Workflow")
+})
 
 app.listen(4000, () => {
     console.log("Server is running on port 3000")
